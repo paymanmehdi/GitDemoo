f982d06efd03d55c94ea0ea0ec5b05542f30b906
Payman <pmg6585@yahoo.com>
2020-10-16 12:57:19 -0400

    first one

---
+++ src/main/java/qaclickadademy/AppiumTest.java
@@ -0,0 +1,14 @@
+package qaclickadademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void NativeAppandroid() {
+		System.out.println("NativeAppandroid");
+	}
+    @Test
+      public void IOSApps() {
+	System.out.println("IOSApps");
+}
+}
